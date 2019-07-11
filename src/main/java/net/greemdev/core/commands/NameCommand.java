217ee6bc10d22b870a33872efe5439be96e4ba52
Michael Husted <gr33m11@gmail.com>
2019-07-10 23:15:08 -0500

    fix permission checks in name command

--- src/main/java/net/greemdev/core/commands/NameCommand.java
+++ src/main/java/net/greemdev/core/commands/NameCommand.java
@@ -12,7 +12,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 public class NameCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!sender.hasPermission("core.name") || !sender.isOp()) {
+        if (!(sender.hasPermission("core.name") || sender.isOp())) {
             sender.sendMessage(ChatColor.DARK_RED + "You do not have access to that command.");
             return true;
         }
@@ -26,11 +26,19 @@ public class NameCommand implements CommandExecutor {
         }
 
         ItemStack item = Bukkit.getPlayer(sender.getName()).getInventory().getItemInMainHand();
+
         if (item == null) {
             sender.sendMessage(ChatColor.DARK_RED + "You're not holding anything.");
             return true;
         }
+
         ItemMeta meta = item.getItemMeta();
+
+        if (meta == null) {
+            sender.sendMessage(ChatColor.DARK_RED + "You're not holding anything.");
+            return true;
+        }
+
         meta.setDisplayName(String.join(" ", args));
         item.setItemMeta(meta);
         sender.sendMessage(ChatColor.DARK_AQUA + "Successfully set the item's name in your main hand to " + String.join(" ", args));
